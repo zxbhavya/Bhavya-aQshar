d0bc8c685a40f4fa22a818cd9a7c492cc9d577c9
Sanskriti Vidushi <vidushisanskriti@gmail.com>
2024-03-31 22:09:58 +0530

    changed settings

--- aqshar/settings.py
+++ aqshar/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-_kdki0&$35q=$&ch@%as4fk3hy7&+)i$m04e6q3cnb^gd)p3te
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'item_images')
 # MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
