b2edccc79648511b346cd960d7c88634ae004cfa
sanskritividushi <118161229+sanskritividushi@users.noreply.github.com>
2024-04-01 12:35:57 +0530

    Update urls.py

--- aqshar/urls.py
+++ aqshar/urls.py
@@ -1,26 +1,22 @@
-
 from django.contrib import admin
-from django.urls import path
-from . import settings
-from core import views
 from django.urls import path, include
-from core.views import chat, index, login_user, catalogue, signup, checkout, selling, account, orders, donating
+from core import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", views.index, name="index"),
     path("core/", include("core.urls")),
-    path("chat/", chat, name="chat"),
-    path("login/", login_user, name="login"),
-    path("catalog/", catalogue, name="catalog"),
-    path("signup/", signup, name="signup"),
-    path("checkout/",checkout, name="checkout"),
-    path("donate/", donating, name="donating"),
-    path('sell/', selling, name='selling'),
-    path('account/', account, name='account'),
-    path('orders', orders, name='orders'),
+    path("chat/", views.chat, name="chat"),
+    path("login/", views.login_user, name="login"),
+    path("catalog/", views.catalogue, name="catalog"),
+    path("signup/", views.signup, name="signup"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("donate/", views.donating, name="donating"),
+    path("sell/", views.selling, name="selling"),
+    path("account/", views.account, name="account"),
+    path("orders/", views.orders, name="orders"),  # Added trailing slash
     path("admin/", admin.site.urls),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
