3cc918ffe4f7341e39531518120c85baba9848e7
sanskritividushi <118161229+sanskritividushi@users.noreply.github.com>
2024-04-01 13:58:29 +0530

    Update settings.py

--- aqshar/settings.py
+++ aqshar/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = "aqshar.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates/core')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
